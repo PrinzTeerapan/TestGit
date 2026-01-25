66a9e270c40db1913dfabd3a908ca75ba855ba54
PrinzTeerapan <teerapan.b@gmail.com>
2026-01-25 20:04:19 +0700

    test

---
+++ Lesson/26Operator.py
@@ -0,0 +1,5 @@
+money = 150
+incomePerDay = 300
+costPerDay = 120
+result = money + (incomePerDay - costPerDay) * 30
+print("At the end of the month, you will have", result, "THB")
---
+++ Lesson/29AssignmentOperator.py
@@ -0,0 +1,14 @@
+#a = 10
+#a +=5           # += คือ a = a + 5
+#print(a)
+
+
+money = 100
+money = money - 20    # money -= 20
+money = money + 10    # money += 10
+print(money)
+
+money2 = 100
+money2 -= 20    # money2 -= 20
+money2 += 10    # money2 += 10
+print(money2)
