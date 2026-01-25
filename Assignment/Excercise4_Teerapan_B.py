09f58c37ce4404f176471b3e456c2096d7620c55
PrinzTeerapan <teerapan.b@gmail.com>
2026-01-25 14:15:02 +0700

    All finished

---
+++ Assignment/Excercise4_Teerapan_B.py
@@ -0,0 +1,9 @@
+FE = 90       #Foundation English
+GB = 89.5     #General Business
+IC = 93.5     #Introduction to Computer Systems
+CP = 95       #Computer Programing
+print("--- Your Score ---")
+print("Foundation English                 : ", FE)
+print("General Business                   : ", GB)
+print("Introduction to Computer Systems   : ", IC)
+print("Computer Programing                : ", CP)
